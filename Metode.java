69641bb341d24dc831414a7187e7408c6d7bb41b
Stefan Njegomirovic <stefannjegomirovic@gmail.com>
2017-12-09 14:39:37 +0100

    dodan random odabir metoda

--- Metode.java
+++ Metode.java
@@ -1,5 +1,5 @@
 package kviz_matematika;
-
+import java.util.Random;
 public class Metode {
 	//Mladen
 	public static double sabiranje(double a, double b) {
@@ -34,12 +34,18 @@ public class Metode {
 	
 	public static void main(String[] args) {
 		//Napravi Stefane switch da se nesto od ovoga nasumicno izabere
-		System.out.println(mnozenje(3, 5));
-		System.out.println(dijeljenje(6, 4));
-		System.out.println(sabiranje(1, 1));
-		System.out.println(oduzimanje(2, 4));
-		System.out.println(korjenovanje(4));
-		System.out.println(kvadriranje(5));
+		Random rand = new Random();
+		int br = rand.nextInt(6);
+		switch (br){
+		case 0:System.out.println(mnozenje(3, 5)); break;
+		case 1:System.out.println(dijeljenje(6, 4));break;
+		case 2:System.out.println(sabiranje(1, 1)); break;
+		case 3:System.out.println(oduzimanje(2, 4));break;
+		case 4:System.out.println(korjenovanje(4)); break;
+		case 5:System.out.println(kvadriranje(5)); break;
+		default: System.out.println("Pogresan unos.");
+		}
+		
 	}
 
 }
